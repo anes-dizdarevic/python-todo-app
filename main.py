3f0171be147d604dc4e9582c920fc1a085b9afbc
anes-dizdarevic <anes11.dizdarevic02@gmail.com>
2026-08-19 12:33:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ root.geometry("400x400")
 root.title("Python Todo List")
 
 #Creates an instruction label
-instruction_label = ttk.Label(root, text="Please input your task into the box below and press the 'Add' button below to add the task to your list")
+instruction_label = ttk.Label(root, wraplength=380, text="Please input your task into the box below and press the 'Add' button below to add the task to your list")
 instruction_label.pack(padx=20, pady=20)
 
 #Creates an entry box
@@ -35,7 +35,9 @@ action_add_line.pack(padx=10, pady=10)
 add_button = ttk.Button(root, text="Add", command=add_new_task)
 add_button.pack(padx=5, pady=5)
 
-
+#Add the delete notice
+delete_label = ttk.Label(root, wraplength=380, text="If you wish to delete your tasks, simply press on the task you wish to delete. Warning: Deleted tasks cannot be recovered.")
+delete_label.pack(padx=5, pady=5)
 
 
 root.mainloop()
